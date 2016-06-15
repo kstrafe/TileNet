24c334042e7412cb24ea015bf302cec6890ee9ea
Kevin Robert Stravers <kstraver@cisco.com>
2016-06-15 17:24:49 +0200

    Implement some tests for occupation

--- src/lib.rs
+++ src/lib.rs
@@ -4,64 +4,83 @@
 /// the map in a single array. Uses T::default() for empty
 /// elements
 struct Map<T> {
-	map: Vec<T>,
+	map: Vec<Option<T>>,
 	cols: usize,
 }
 
-impl<T> Map<T> {
-	fn get(&self, p: (usize, usize)) -> Option<&T> {
-		self.map.get(p.0 + p.1*self.cols)
-	}
-
-	fn get_mut(&mut self, p: (usize, usize)) -> Option<&mut T> {
-		self.map.get_mut(p.0 + p.1*self.cols)
-	}
-
-	fn is_occupied(&self, p: (usize, usize)) {
-		self.get(p).is_some();
-	}
-}
-
-struct RangedMap<'a, T: 'a> {
-	map: &'a Map<T>,
-}
-
-impl<T> Map<T>
-where T: std::default::Default + std::clone::Clone {
-	fn new() -> Map<T> {
+impl<T> Map<T> where T: Clone {
+	fn new(m: (usize, usize)) -> Map<T> {
 		Map {
-			map: vec![],
-			cols: 0,
+			map: vec![None; m.0*m.1],
+			cols: m.1,
 		}
 	}
 
-	fn resize(&mut self, rows: usize, cols: usize) {
-		self.map = vec![T::default(); rows*cols];
-		self.cols = cols;
-	}
-
-	fn range(&self, left: usize, right: usize, bottom: usize, top: usize) -> RangedMap<T> {
-		RangedMap {
-			map: self,
+	fn get(&self, p: (usize, usize)) -> Option<&Option<T>> {
+		if p.0 >= self.cols {
+			None
+		} else {
+			self.map.get(p.0 + p.1*self.cols)
 		}
 	}
-}
 
-#[derive(Clone)]
-enum Tile {
-	Empty,
-	Dirt,
-}
+	fn get_mut(&mut self, p: (usize, usize)) -> Option<&mut Option<T>> {
+		if p.0 >= self.cols {
+			None
+		} else {
+			self.map.get_mut(p.0 + p.1*self.cols)
+		}
+	}
 
-impl std::default::Default for Tile {
-	fn default() -> Tile {
-		Tile::Empty
+	fn is_occupied(&self, p: (usize, usize)) -> bool {
+		match self.get(p) {
+			Some(tile) => tile.is_some(),
+			None => true,
+		}
+	}
+
+	fn any_occupied(&self, list: Vec<(usize, usize)>) -> bool {
+		list.iter().any(|&p| self.is_occupied(p))
 	}
 }
 
 #[cfg(test)]
 mod tests {
+	use super::Map;
+
 	#[test]
-	fn test() {
+	fn get() {
+		let map: Map<usize> = Map::new((10, 10));
+		assert_eq!(Some(&None), map.get((9, 9)));
+		assert_eq!(None, map.get((10, 9)));
+		assert_eq!(None, map.get((9, 10)));
+	}
+
+	#[test]
+	fn is_occupied() {
+		let mut map: Map<usize> = Map::new((10, 10));
+		{
+			let element = map.get_mut((9, 9));
+			*element.unwrap() = Some(0);
+		}
+		assert_eq!(true, map.is_occupied((9, 9)));
+		assert_eq!(false, map.is_occupied((0, 0)));
+		assert_eq!(true, map.is_occupied((10, 0)));
+	}
+
+	#[test]
+	fn any_occupied() {
+		let mut map: Map<usize> = Map::new((10, 10));
+		{
+			let element = map.get_mut((9, 9));
+			*element.unwrap() = Some(0);
+		}
+		assert_eq!(true, map.any_occupied(vec![
+			(9, 9),
+			(10, 0),
+		]));
+		assert_eq!(false, map.any_occupied(
+			(0..10).map(|x| (0, x)).collect::<Vec<_>>()
+		));
 	}
 }
