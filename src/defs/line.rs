825729393006c6c010d2b1dabb4a99a8b3abf99e
Bourgond Aries <macocio@gmail.com>
2016-06-23 00:00:54 +0200

    Prove top-down algorithim

--- src/defs/line.rs
+++ src/defs/line.rs
@@ -201,6 +201,7 @@ mod tests {
 
 	#[test]
 	fn supercover() {
+		assert!(seq((2.0, 2.0), vec![(0i32, 0i32), (0, 1), (1, 1), (1, 2), (2, 2)].iter().map(|x| *x)));
 		assert!(seq((-2.0, 0.0), (0..3).map(|x| (-x, 0))));
 		assert!(seq((0.0, -0.1), (0..2).map(|x| (0, -x))));
 		assert!(seq((0.0, 0.1), (0..1).map(|_| (0, 0))));
